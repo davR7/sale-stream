ca8d04e72f0a30f8aee3238b1ba90daaea0e668a
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-13 17:37:48 -0300

    added - @EnableJpaAuditing annotation

--- src/main/java/com/davr7/salestream/SaleStreamApplication.java
+++ src/main/java/com/davr7/salestream/SaleStreamApplication.java
@@ -2,8 +2,10 @@ package com.davr7.salestream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SaleStreamApplication {
 
 	public static void main(String[] args) {
