be62e971bed818dc12a21360c4cc0ade56ffb5f8
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-13 15:56:06 -0300

    created - first commit

---
+++ src/main/java/com/davr7/salestream/SaleStreamApplication.java
@@ -0,0 +1,13 @@
+package com.davr7.salestream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaleStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davr7/salestream/SaleStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davr7.salestream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
