0a0c58a39b4fac80738e8c8b95890ea40ef55eb6
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-15 10:20:52 -0300

    added - @Service annotation to the CustomerService

--- src/main/java/com/davr7/salestream/services/CustomerService.java
+++ src/main/java/com/davr7/salestream/services/CustomerService.java
@@ -1,12 +1,13 @@
 package com.davr7.salestream.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.davr7.salestream.repositories.CustomerRepository;
 
+@Service
 public class CustomerService {
 	
 	@Autowired
-	CustomerRepository customerRepo;	
-
+	CustomerRepository customerRepo;
 }
