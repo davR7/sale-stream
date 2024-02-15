2fd8e11191e099e8e28c735ef04c1cde427e9254
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-15 17:03:25 -0300

    added - orders property and @OneToMany annotation

--- src/main/java/com/davr7/salestream/entities/Customer.java
+++ src/main/java/com/davr7/salestream/entities/Customer.java
@@ -1,12 +1,17 @@
 package com.davr7.salestream.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,17 +23,23 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
-@EqualsAndHashCode(of="id", callSuper = true)
-@Builder
+@EqualsAndHashCode(of = "id", callSuper = true)
 public class Customer extends User {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.UUID)
 	private String id;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "customer", cascade = CascadeType.ALL)
+	private Set<Order> orders = new HashSet<>(); 
 
 	public Customer(String id, String fullname, String username, String email, String phone, String password) {
 		super(fullname, username, email, phone, password);
 		this.id = id;
+	}
+	
+	public Set<Order> getOrders() {
+		return orders;
 	}
 }
