c0cda989b201b6e0cc9c728ccf2ffd7ac13338a2
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-15 11:54:22 -0300

    added - createdAt and updatedAt properties to the User class

--- src/main/java/com/davr7/salestream/entities/User.java
+++ src/main/java/com/davr7/salestream/entities/User.java
@@ -1,19 +1,29 @@
 package com.davr7.salestream.entities;
 
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@MappedSuperclass
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-public abstract class User extends DateAudit {
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	protected String fullname;
@@ -22,4 +32,17 @@ public abstract class User extends DateAudit {
 	protected String phone;
 	@JsonIgnore
 	protected String password;
+	@CreatedDate
+    @Column(nullable = false, updatable = false)
+    protected LocalDateTime createdAt;
+    @LastModifiedDate
+    protected LocalDateTime updatedAt;
+	
+    public User(String fullname, String username, String email, String phone, String password) {
+		this.fullname = fullname;
+		this.username = username;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
 }
