fb191c17d8bc29387af7dd934161ee3219680d84
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-15 10:21:34 -0300

    added - findAllCustomers method to the CustomerService class

--- src/main/java/com/davr7/salestream/services/CustomerService.java
+++ src/main/java/com/davr7/salestream/services/CustomerService.java
@@ -1,8 +1,11 @@
 package com.davr7.salestream.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.davr7.salestream.entities.Customer;
 import com.davr7.salestream.repositories.CustomerRepository;
 
 @Service
@@ -10,4 +13,8 @@ public class CustomerService {
 	
 	@Autowired
 	CustomerRepository customerRepo;
+	
+	public List<Customer> findAllCustomers() {
+		return customerRepo.findAll();
+	}
 }
