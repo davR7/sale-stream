6ba2c2e5a0bed7feaaadc9d8463a011437cdbdbd
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-15 10:33:54 -0300

    added - handlerFindAllCustomers method to the CustomerResource class

--- src/main/java/com/davr7/salestream/resources/CustomerResource.java
+++ src/main/java/com/davr7/salestream/resources/CustomerResource.java
@@ -1,9 +1,14 @@
 package com.davr7.salestream.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.davr7.salestream.entities.Customer;
 import com.davr7.salestream.services.CustomerService;
 
 @RestController
@@ -12,4 +17,10 @@ public class CustomerResource {
 	
 	@Autowired
 	CustomerService customerServ;
+	
+	@GetMapping
+	public ResponseEntity<List<Customer>> handlerFindAllCustomers() {
+		List<Customer> list = customerServ.findAllCustomers();
+		return ResponseEntity.ok().body(list);
+	}
 }
