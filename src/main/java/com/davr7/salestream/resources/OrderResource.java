29b9f67fd617877b2afaee64f13915a46389eba0
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-16 18:12:32 -0300

    added - handlerFindAllOrders method to the OrderResource

--- src/main/java/com/davr7/salestream/resources/OrderResource.java
+++ src/main/java/com/davr7/salestream/resources/OrderResource.java
@@ -1,9 +1,14 @@
 package com.davr7.salestream.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.davr7.salestream.entities.Order;
 import com.davr7.salestream.services.OrderService;
 
 @RestController
@@ -12,4 +17,10 @@ public class OrderResource {
 	
 	@Autowired
 	OrderService orderServ;
+	
+	@GetMapping
+	public ResponseEntity<List<Order>> handlerFindAllOrders() {
+		List<Order> list = orderServ.findAllOrders();
+		return ResponseEntity.ok().body(list);
+	}
 }
