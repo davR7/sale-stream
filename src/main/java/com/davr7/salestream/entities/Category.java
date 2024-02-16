2f9826725d449d42d415fe7be9dd9f6a1beea1fe
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-16 20:15:21 -0300

    added - products property and @ManyToMany annotation

--- src/main/java/com/davr7/salestream/entities/Category.java
+++ src/main/java/com/davr7/salestream/entities/Category.java
@@ -1,11 +1,16 @@
 package com.davr7.salestream.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -26,6 +31,9 @@ public class Category implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.UUID)
 	private String id;
-	
 	private String name;
+	
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products =  new HashSet<>();
 }
