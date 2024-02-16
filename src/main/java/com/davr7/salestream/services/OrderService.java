50d36c8be218d2225344b7b4b58f904619a50c27
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-16 18:07:01 -0300

    added - findAllOrders method to the OrderService class

--- src/main/java/com/davr7/salestream/services/OrderService.java
+++ src/main/java/com/davr7/salestream/services/OrderService.java
@@ -1,8 +1,11 @@
 package com.davr7.salestream.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.davr7.salestream.entities.Order;
 import com.davr7.salestream.repositories.OrderRepository;
 
 @Service
@@ -10,4 +13,8 @@ public class OrderService {
 	
 	@Autowired
 	OrderRepository orderRepo;
+	
+	public List<Order> findAllOrders() {
+		return orderRepo.findAll();
+	}
 }
