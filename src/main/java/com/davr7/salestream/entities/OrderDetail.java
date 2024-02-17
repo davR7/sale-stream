eac5ae9db4b1fb78091e43b4110d19ff44f3266b
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-17 13:53:58 -0300

    created - OrderDetail entity class

---
+++ src/main/java/com/davr7/salestream/entities/OrderDetail.java
@@ -0,0 +1,57 @@
+package com.davr7.salestream.entities;
+
+import java.io.Serializable;
+
+import com.davr7.salestream.entities.pk.OrderDetailPK;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="order_detail")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of="id")
+public class OrderDetail implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Getter(value = AccessLevel.NONE)
+	@Setter(value = AccessLevel.NONE)
+	@EmbeddedId
+	private OrderDetailPK id;
+	
+	private Integer quantity;
+	private Double price;
+	
+	public OrderDetail(Order order, Product product, Integer quantity, Double price) {
+		this.quantity = quantity;
+		this.price = price;
+		id.setOrder(order);
+		id.setProduct(product);
+	}
+	
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+	
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	
+	public void setProduct(Product product) {
+		id.setProduct(product);
+	}
+}
