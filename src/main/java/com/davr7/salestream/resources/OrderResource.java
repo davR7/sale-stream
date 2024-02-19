dbdaadb7867128f7b396dcef7d5ab7e26c15c200
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-19 14:05:10 -0300

    added - handleFindOrderById to the OrderResource class

--- src/main/java/com/davr7/salestream/resources/OrderResource.java
+++ src/main/java/com/davr7/salestream/resources/OrderResource.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.davr7.salestream.entities.Category;
 import com.davr7.salestream.entities.Order;
 import com.davr7.salestream.services.OrderService;
 
@@ -17,6 +19,12 @@ public class OrderResource {
 	
 	@Autowired
 	OrderService orderServ;
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<Order> handleFindOrderById(@PathVariable String id) {
+		Order obj = orderServ.findOrderById(id);
+		return ResponseEntity.ok().body(obj);
+	}
 	
 	@GetMapping
 	public ResponseEntity<List<Order>> handlerFindAllOrders() {
