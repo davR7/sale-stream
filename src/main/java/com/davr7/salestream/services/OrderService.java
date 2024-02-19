3ea12348418d6c8e79ca1c4a849605b01875cb3b
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-19 13:33:18 -0300

    added - findOrderById to the OrderService class

--- src/main/java/com/davr7/salestream/services/OrderService.java
+++ src/main/java/com/davr7/salestream/services/OrderService.java
@@ -1,18 +1,25 @@
 package com.davr7.salestream.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.davr7.salestream.entities.Order;
 import com.davr7.salestream.repositories.OrderRepository;
+import com.davr7.salestream.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class OrderService {
 	
 	@Autowired
 	OrderRepository orderRepo;
+	
+	public Order findOrderById(String id) {
+		Optional<Order> data = orderRepo.findById(id);
+		return data.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
 	
 	public List<Order> findAllOrders() {
 		return orderRepo.findAll();
