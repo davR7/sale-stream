26c46f3ac42e138b31cec9dfad5be842c34ccec6
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-19 14:14:09 -0300

    added - handleFindProductById to the ProductResource class

--- src/main/java/com/davr7/salestream/resources/ProductResource.java
+++ src/main/java/com/davr7/salestream/resources/ProductResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,12 @@ public class ProductResource {
 	
 	@Autowired
 	ProductService productServ;
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<Product> handleFindProductById(@PathVariable String id) {
+		Product obj = productServ.findProductById(id);
+		return ResponseEntity.ok().body(obj);
+	}
 	
 	@GetMapping
 	public ResponseEntity<List<Product>> handlerFindAllCategories() {
