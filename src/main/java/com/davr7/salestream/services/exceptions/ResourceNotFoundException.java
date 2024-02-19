ac474070472dddb3d9bf9009c8efd73a6e046896
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-19 13:21:34 -0300

    added - constructor public to the ResourceNotFoundException class

--- src/main/java/com/davr7/salestream/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/davr7/salestream/services/exceptions/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package com.davr7.salestream.services.exceptions;
 public class ResourceNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
-	ResourceNotFoundException(String id){
+	public ResourceNotFoundException(String id){
 		super("Resource not found: "+id);
 	};
 }
