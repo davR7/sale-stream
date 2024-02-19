fa44c4706ba3bb26ce828c5e63611b4fc3c1960a
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-19 13:31:12 -0300

    added - findCategoryById to the CategoryService class

--- src/main/java/com/davr7/salestream/services/CategoryService.java
+++ src/main/java/com/davr7/salestream/services/CategoryService.java
@@ -1,18 +1,25 @@
 package com.davr7.salestream.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.davr7.salestream.entities.Category;
 import com.davr7.salestream.repositories.CategoryRepository;
+import com.davr7.salestream.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
 	
 	@Autowired
 	CategoryRepository categoryRepo;
+	
+	public Category findCategoryById(String id) {
+		Optional<Category> data = categoryRepo.findById(id);
+		return data.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
 	
 	public List<Category> findAllCategories() {
 		return categoryRepo.findAll();
