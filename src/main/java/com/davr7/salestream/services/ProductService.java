a94b6d0b8418244bfd82197d6e98e2e5049719ea
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-19 13:34:54 -0300

    added - findProductById to the ProductService class

--- src/main/java/com/davr7/salestream/services/ProductService.java
+++ src/main/java/com/davr7/salestream/services/ProductService.java
@@ -1,18 +1,25 @@
 package com.davr7.salestream.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.davr7.salestream.entities.Product;
 import com.davr7.salestream.repositories.ProductRepository;
+import com.davr7.salestream.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
 	
 	@Autowired
 	ProductRepository productRepo;
+	
+	public Product findProductById(String id) {
+		Optional<Product> data = productRepo.findById(id);
+		return data.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
 	
 	public List<Product> findAllProducts() {
 		return productRepo.findAll();
