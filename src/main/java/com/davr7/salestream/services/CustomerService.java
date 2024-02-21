b6945359a09a2067f479b4784d33d5c176e4d690
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-21 12:01:14 -0300

    updated - findAllCustomers method of the CustomerService return List<CustomerDTO>

--- src/main/java/com/davr7/salestream/services/CustomerService.java
+++ src/main/java/com/davr7/salestream/services/CustomerService.java
@@ -2,10 +2,12 @@ package com.davr7.salestream.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.davr7.salestream.dtos.CustomerDTO;
 import com.davr7.salestream.entities.Customer;
 import com.davr7.salestream.repositories.CustomerRepository;
 import com.davr7.salestream.services.exceptions.ResourceNotFoundException;
@@ -21,7 +23,11 @@ public class CustomerService {
 		return data.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
-	public List<Customer> findAllCustomers() {
-		return customerRepo.findAll();
+	public List<CustomerDTO> findAllCustomers() {
+		List<Customer> list = customerRepo.findAll();
+		List<CustomerDTO> listDTO = list.stream()
+				.map(c -> CustomerDTO.create(c))
+				.collect(Collectors.toList());
+		return listDTO;
 	}
 }
