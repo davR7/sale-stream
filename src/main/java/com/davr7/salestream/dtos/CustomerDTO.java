4b3db2aea9c50ba505fa46a16c6b0b487a6dd368
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-21 16:04:34 -0300

    updated - removed create method to the CustomerDTO

--- src/main/java/com/davr7/salestream/dtos/CustomerDTO.java
+++ src/main/java/com/davr7/salestream/dtos/CustomerDTO.java
@@ -5,8 +5,4 @@ import java.time.LocalDateTime;
 import com.davr7.salestream.entities.Customer;
 
 public record CustomerDTO(String id, String fullname, String username, String email, String phone, LocalDateTime createdAt, LocalDateTime  updatedAt) {
-
-	public static CustomerDTO create(Customer c) {
-		return new CustomerDTO(c.getId() ,c.getFullname(), c.getUsername(), c.getEmail(), c.getPhone(), c.getCreatedAt(), c.getUpdatedAt());
-	}
 }
