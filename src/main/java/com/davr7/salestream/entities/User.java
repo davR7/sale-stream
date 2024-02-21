a73874e2829282af0bc928b47e792d78c0186f93
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-21 11:33:24 -0300

    added - small fixes

--- src/main/java/com/davr7/salestream/entities/User.java
+++ src/main/java/com/davr7/salestream/entities/User.java
@@ -1,16 +1,5 @@
 package com.davr7.salestream.entities;
 
-import java.io.Serializable;
-import java.time.LocalDateTime;
-
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,27 +11,12 @@ import lombok.Setter;
 @Getter
 @Setter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
-public abstract class User implements Serializable {
+public abstract class User extends DateAudit {
 	private static final long serialVersionUID = 1L;
 	
 	protected String fullname;
 	protected String username;
 	protected String email;
 	protected String phone;
-	@JsonIgnore
 	protected String password;
-	@CreatedDate
-    @Column(nullable = false, updatable = false)
-    protected LocalDateTime createdAt;
-    @LastModifiedDate
-    protected LocalDateTime updatedAt;
-	
-    public User(String fullname, String username, String email, String phone, String password) {
-		this.fullname = fullname;
-		this.username = username;
-		this.email = email;
-		this.phone = phone;
-		this.password = password;
-	}
 }
