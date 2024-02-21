08f42807abbd45602e4ebda1856dbeb6f1ccea8f
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-21 16:15:40 -0300

    updated - small fixes

--- src/main/java/com/davr7/salestream/dtos/CustomerDTO.java
+++ src/main/java/com/davr7/salestream/dtos/CustomerDTO.java
@@ -2,7 +2,5 @@ package com.davr7.salestream.dtos;
 
 import java.time.LocalDateTime;
 
-import com.davr7.salestream.entities.Customer;
-
 public record CustomerDTO(String id, String fullname, String username, String email, String phone, LocalDateTime createdAt, LocalDateTime  updatedAt) {
 }
