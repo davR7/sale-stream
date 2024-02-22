f849f7a1eb6e6141d954de0291fa18a96058ad82
Davidson Ratis <davidsonmakarios@gmail.com>
2024-02-22 08:18:20 -0300

    added - toOrderDTO method of OrderMapper instance

--- src/main/java/com/davr7/salestream/services/OrderService.java
+++ src/main/java/com/davr7/salestream/services/OrderService.java
@@ -2,11 +2,14 @@ package com.davr7.salestream.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.davr7.salestream.dtos.OrderDTO;
 import com.davr7.salestream.entities.Order;
+import com.davr7.salestream.entities.mappers.OrderMapper;
 import com.davr7.salestream.repositories.OrderRepository;
 import com.davr7.salestream.services.exceptions.ResourceNotFoundException;
 
@@ -16,12 +19,16 @@ public class OrderService {
 	@Autowired
 	OrderRepository orderRepo;
 	
-	public Order findOrderById(String id) {
+	public OrderDTO findOrderById(String id) {
 		Optional<Order> data = orderRepo.findById(id);
-		return data.orElseThrow(() -> new ResourceNotFoundException(id));
+		return OrderMapper.INSTANCE.toOrderDTO(data.orElseThrow(
+				() -> new ResourceNotFoundException(id)));
 	}
 	
-	public List<Order> findAllOrders() {
-		return orderRepo.findAll();
+	public List<OrderDTO> findAllOrders() {
+		List<Order> list = orderRepo.findAll();
+		return list.stream()
+				.map(o -> OrderMapper.INSTANCE.toOrderDTO(o))
+				.collect(Collectors.toList());
 	}
 }
